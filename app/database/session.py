54b79dbf476a69ce5f9c90a64d27163d50694ec0
Łukasz Dzięcielewski <lukaszdziecielewski83@gmail.com>
2026-07-27 17:38:23 +0200

    Implement SQLAlchemy session management
    Removed old docstring and added SQLAlchemy engine and session configuration.

--- app/database/session.py
+++ app/database/session.py
@@ -1,6 +1,25 @@
-"""
-Database session management.
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
-This module will contain database connection
-and SQLAlchemy session configuration.
-"""
+from app.core.config import settings
+
+
+DATABASE_URL = (
+    f"postgresql://{settings.database_user}:"
+    f"{settings.database_password}@"
+    f"{settings.database_host}:"
+    f"{settings.database_port}/"
+    f"{settings.database_name}"
+)
+
+
+engine = create_engine(
+    DATABASE_URL
+)
+
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
